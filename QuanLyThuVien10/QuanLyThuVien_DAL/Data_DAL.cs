08b67f1c982b41fb48d66f35967acb1325a96f68
NguyenCongBao <52964408+NguyenCongBao@users.noreply.github.com>
2020-06-03 12:55:09 +0700

    Add files via upload

---
+++ QuanLyThuVien10/QuanLyThuVien_DAL/Data_DAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuanLyThuVien_DAL
+{
+    public class Data_DAL
+    {
+
+    }
+}
---
+++ QuanLyThuVien10/QuanLyThuVien_GUI/runQLTV_GUI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QuanLyThuVien_GUI
+{
+    static class runQLTV_GUI
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new dangNhap_GUI());
+        }
+    }
+}
+    
