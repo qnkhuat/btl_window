bd929a3bdd557d99fa4082c9323ae806b8995abc
NguyenCongBao <52964408+NguyenCongBao@users.noreply.github.com>
2020-06-20 15:18:40 +0700

    ..

--- QuanLyThuVien10/QuanLyThuVien_DAL/Data_DAL.cs
+++ QuanLyThuVien10/QuanLyThuVien_DAL/Data_DAL.cs
@@ -13,7 +13,7 @@ namespace QuanLyThuVien_DAL
     {
         public SqlConnection getConnect()
         {
-            return new SqlConnection(@"Data Source=ADMIN\SQLEXPRESS;Initial Catalog=Quan_Ly_Thu_Vien;Integrated Security=True");
+            return new SqlConnection(@"Data Source=DESKTOP-9O7NFIJ\SQLEXPRESS;Initial Catalog=Quan_Ly_Thu_Vien;Integrated Security=True");
         }
         //Lấy bảng dữ liệu
         public DataTable GetTable(String sql)
